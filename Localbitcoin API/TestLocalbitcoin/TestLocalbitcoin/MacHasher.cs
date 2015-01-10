9a68797e8aa2a77b669266f304d1abd8184ec32b
Philip Pierce <piercep@rocketgamesmobile.com>
2015-01-10 14:26:42 -0500

    working on Localbitcoin API access

---
+++ Localbitcoin API/TestLocalbitcoin/TestLocalbitcoin/MacHasher.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace SC
+{
+    public class MacHasher
+    {
+        #region API_Create_Hash
+
+        /// <summary>
+        /// Creates the hash
+        /// </summary>
+        /// <param name="relativeURL">Relative path, for example /api/wallet/.</param>
+        /// <param name="timeTicksNonce">DateTime.UtcNow.Ticks.ToString()</param>
+        /// <param name="URLparams">GET or POST parameters in their URL encoded format, for example foo=bar&amp;baz=quux.</param>
+        /// <returns></returns>
+        public static string API_Create_Hash(string timeTicksNonce, string secret, string relativeURL, string URLparams)
+        {
+            string message = string.Format("{0}{1}{2}{3}",
+                timeTicksNonce, // nonce
+                secret, // api secret key
+                relativeURL,
+                URLparams);
+
+            // get the bytes of the secret
+            HMACSHA1 hmacsha1 = new HMACSHA1(Encoding.UTF8.GetBytes(secret));
+            
+            // hash the message and the time
+            byte[] hashmessage = hmacsha1.ComputeHash(Encoding.UTF8.GetBytes(message));
+
+            // return the string
+            return BitConverter.ToString(hashmessage);
+        }
+
+        // API_Create_Hash
+        #endregion
+
+        #region ByteToString
+
+        /// <summary>
+        /// Converts a byte array to a string
+        /// </summary>
+        /// <param name="buff"></param>
+        /// <returns></returns>
+        private static string ByteToString(byte[] buff)
+        {
+            StringBuilder sb = new StringBuilder();
+            int j = buff.Length;
+            for (int i = 0; i < j; i++)
+                sb.Append(buff[i].ToString("X2"));
+
+            return sb.ToString();
+        }
+
+        // ByteToString
+        #endregion
+    }
+}
---
+++ Localbitcoin API/TestLocalbitcoin/TestLocalbitcoin/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using SC;
+
+namespace TestLocalbitcoin
+{
+    class Program
+    {
+        const string secret = "e723aef2ea46d9b705f23fb6ed54536b";
+
+        static void Main(string[] args)
+        {
+            /*
+ Signature is sent via HTTP headers. A total of three fields are needed:
+
+    Apiauth-Key: API authentication key.
+    Apiauth-Nonce: The nonce in this particular request.
+    Apiauth-Signature: HMAC signature.
+            */
+
+
+            const string relativeURL = "/api/myself/";
+            string nonce = DateTime.UtcNow.Ticks.ToString();
+
+            // get the mac hash
+            string hash = MacHasher.API_Create_Hash(nonce, secret, relativeURL, string.Empty);
+
+            // build the url
+            string url = string.Format("https://localbitcoins.com{0}", relativeURL);
+
+            // build the header
+            string header = string.Format("Apiauth-Key={0}&Apiauth-Nonce{1}&Apiauth-Signature={2}",
+                secret,
+                nonce,
+                hash);
+
+            Console.WriteLine(url);
+            Console.WriteLine(header);
+            Console.WriteLine(CallToService_GET(url, secret, nonce, hash));
+
+            Console.WriteLine("FINSIHED");
+            Console.ReadLine();
+        }
+
+
+        /// <summary>
+        /// Calls to the webservice
+        /// </summary>
+        /// <returns></returns>
+        public static string CallToService_GET(string URL, string headerKey, string headerNonce, string headerHash)
+        {
+            // set content header and length
+            HttpWebRequest BitcoinWebRequest = (HttpWebRequest)HttpWebRequest.Create(URL);
+            BitcoinWebRequest.Method = "GET";
+
+            BitcoinWebRequest.Headers.Add("Apiauth-Key", headerKey);
+            BitcoinWebRequest.Headers.Add("Apiauth-Nonce", headerNonce);
+            BitcoinWebRequest.Headers.Add("Apiauth-Signature", headerHash);
+
+            // call the service
+            using (HttpWebResponse myHttpWebResponse = (HttpWebResponse)BitcoinWebRequest.GetResponse())
+            {
+                using (Stream responseStream = myHttpWebResponse.GetResponseStream())
+                {
+                    StreamReader myStreamReader = new StreamReader(responseStream, Encoding.UTF8);
+
+                    // read the data
+                    return myStreamReader.ReadToEnd();
+                }
+            }
+        }
+    }
+}

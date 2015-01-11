a8df82569c5f76260f845fca738e73b5b1296293
Philip Pierce <piercep@rocketgamesmobile.com>
2015-01-11 00:18:36 -0500

    server side update, atm UI, and mobile update

--- AbeATM/AbeATM/Default.aspx.cs
+++ AbeATM/AbeATM/Default.aspx.cs
@@ -12,9 +12,26 @@ namespace AbeATM
         #region Variables
 
         // TODO: these need to go to the viewstate
+        private static float BtcQuantityToSell = 0;
+        private static bool IsBtcSellStep1 = true;
 
+        // TODO: get this exchange rate from the server
+        private float OneBtcRate = 290;
 
         // Variables
+        #endregion
+
+        #region Page_Load
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (! IsPostBack)
+            {
+                // do reset
+                BtcQuantityToSell = 0;
+            }
+        }
+
         #endregion
 
         #region HidePanels
@@ -46,10 +63,51 @@ namespace AbeATM
             this.lblSellHowMany.Text = "How many <strong>Bitcoins</strong> to sell";
             this.lblSellBtcCurrencyMark.Text = "BTC&nbsp;";
             this.txtSellQuantity.Text = string.Empty;
+            BtcQuantityToSell = 0;
+            IsBtcSellStep1 = true;
+
 
             this.pnlSell_Step1.Visible = true;
         }
 
+        #endregion
+
+        #region cmdSetBtcSellQuantity_Click
+
+        /// <summary>
+        /// user clicked btc sell amount - Next
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void cmdSetBtcSellQuantity_Click(object sender, EventArgs e)
+        {
+            // exit if nothing in text
+            if (string.IsNullOrEmpty(this.txtSellQuantity.Text))
+                return;
+
+            // BTC to sell
+            if (IsBtcSellStep1)
+            {
+                // exit if we can't convert
+                float BtcToSell = 0;
+                if (!float.TryParse(this.txtSellQuantity.Text, out BtcToSell))
+                    return;
+
+                // set the value
+                BtcQuantityToSell = BtcToSell;
+
+                // set the labels so we can go to step 2
+                this.lblSellHowMany.Text = "How much to sell them for?";
+                this.lblSellBtcCurrencyMark.Text = "$&nbsp;";
+                this.txtSellQuantity.Text = (OneBtcRate * BtcQuantityToSell).ToString("N2");
+                BtcQuantityToSell = 0;
+                IsBtcSellStep1 = false;
+                return;
+            }
+
+            // step 2 of sell
+            // TODO: save and load next panel
+        }
         #endregion
     }
 }
